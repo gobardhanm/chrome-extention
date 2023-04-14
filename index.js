14960a1b62b88624350a3ca286baeda17a996072
gobardhanm <gobardhanms364@gmail.com>
2023-04-14 23:21:33 +0530

    ext

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// 
+let myLeads = [];
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+})
+
+for(let i= 0; i < myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
+
