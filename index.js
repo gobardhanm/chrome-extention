f9819d0699d1b3d6f524d707352589928a584ab6
gobardhanm <gobardhanms364@gmail.com>
2023-04-15 23:28:27 +0530

    extension

--- index.js
+++ index.js
@@ -4,11 +4,40 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
+    inputEl.value= "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads();
 })
 
-for(let i= 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads(){
+        let listItems = []
+    for(let i= 0; i < myLeads.length; i++){
+    
+        // listItems += "<li><a  target=_blank href='"+myLeads[i]+"'>" + myLeads[i] + "</li>"
+
+        // Template String or Literal
+        listItems += 
+        `<li>
+            <a target=_blank href=" ${myLeads[i]} ">
+             ${myLeads[i]}
+            </a> 
+        </li>`
+
+
+        // ANOTHER WAY :
+        //create element
+        // // set textcontent
+        // // append to ul
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    
+    ulEl.innerHTML = listItems
 }
 
