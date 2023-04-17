1a782c7141d9a03c0c0787da43c0673ac2071299
gobardhanm <gobardhanms364@gmail.com>
2023-04-17 21:22:57 +0530

    URL Saver chrome extension d & d

--- index.js
+++ index.js
@@ -13,14 +13,13 @@ if(leadsFromLocalStorage){
     renderLeads(myLeads)
 }
 
-const tabs =[
-    {url: "https://www.linkedin.com/in/gerrymeher"}
-]
-
 saveTab.addEventListener("click", function(){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+    
 })
 
 function render(leads){
